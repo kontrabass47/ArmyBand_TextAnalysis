1fac11af5922c5fd826e5ca713f09389d7114e55
dariencupit <dariencupit1@gmail.com>
2019-07-03 15:41:32 -0400

    moved existing code inside functions

--- src/vader_impl.py
+++ src/vader_impl.py
@@ -3,20 +3,28 @@
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-print("All inputs are case sensitive!")
-file = input("Please enter the path to the .xlsx file to parse: ")
-column = input("Please enter the name of the column with text to parse: ")
 
-df_wp=pd.read_excel(file)
-df_wp.dropna()
 
-df_wplist=df_wp[column].tolist()
+def main():
+    print("All inputs are case sensitive!")
+    file = input("Please enter the path to the .xlsx file to parse: ")
+    column = input("Please enter the name of the column with text to parse: ")
 
-analyzer=SentimentIntensityAnalyzer()
+    df_wp=pd.read_excel(file)
+    df_wp.dropna()
 
-counter = 0
-for sentence in df_wplist:
-    vs=analyzer.polarity_scores(sentence)
-    scores=("Sentence {}: {}".format(counter, str(vs)))
-    print(scores)
-    counter += 1
+    df_wplist=df_wp[column].tolist()
+
+    analyzer=SentimentIntensityAnalyzer()
+
+    counter = 0
+    for sentence in df_wplist:
+        vs=analyzer.polarity_scores(sentence)
+        scores=("Sentence {}: {}".format(counter, str(vs)))
+        print(scores + " ")
+        counter += 1
+
+
+if __name__ == "__main__":
+    main()
+
