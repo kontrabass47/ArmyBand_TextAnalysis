d867d0bf147abbf1bacfe64fe6fa931dfe314189
dariencupit <dariencupit1@gmail.com>
2019-07-08 13:24:00 -0400

    Fixed minor error with reading files

--- src/vader_impl.py
+++ src/vader_impl.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-import pandas as pd
-from .text_analysis import TextAnalysis
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-class Vader(TextAnalysis):
+class Vader():
     def __init__(self):
         self.sentimentList = [] # Contains as list of vaderSentiment objects
 
@@ -12,18 +10,11 @@ class Vader(TextAnalysis):
 
     def analyzeFile(self, list):
         print("All inputs are case sensitive!")
-        file = input("Please enter the path to the .xlsx file to parse: ")
-        column = input("Please enter the name of the column with text to parse: ")
-
-        df_wp = pd.read_excel(file)
-        df_wp.dropna()
-
-        df_wplist = df_wp[column].tolist()
 
         analyzer = SentimentIntensityAnalyzer()
 
         counter = 0
-        for sentence in df_wplist:
+        for sentence in list:
             vs = analyzer.polarity_scores(sentence)
             self.sentimentList.append(vs)
             scores = ("Sentence {}: {}".format(counter, str(vs)))
