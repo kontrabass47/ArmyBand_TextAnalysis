c5ca1c1121b437609b44e8e4f81fb63657c041bc
dariencupit <dariencupit1@gmail.com>
2019-07-08 10:42:11 -0400

    Added basic class functionality for vader_impl.py

--- src/vader_impl.py
+++ src/vader_impl.py
@@ -4,30 +4,26 @@ import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 class Vader:
-    sentimentList =[] # Contains as list of vaderSentiment objects
+    vaderSentimentList = [] # Contains as list of vaderSentiment objects
 
-def main():
-    vader = Vader()
-    print("All inputs are case sensitive!")
-    file = input("Please enter the path to the .xlsx file to parse: ")
-    column = input("Please enter the name of the column with text to parse: ")
+    def vaderAnalyzer(self):
+        vader = Vader()
+        print("All inputs are case sensitive!")
+        file = input("Please enter the path to the .xlsx file to parse: ")
+        column = input("Please enter the name of the column with text to parse: ")
 
-    df_wp=pd.read_excel(file)
-    df_wp.dropna()
+        df_wp = pd.read_excel(file)
+        df_wp.dropna()
 
-    df_wplist=df_wp[column].tolist()
+        df_wplist = df_wp[column].tolist()
 
-    analyzer=SentimentIntensityAnalyzer()
+        analyzer = SentimentIntensityAnalyzer()
 
-    counter = 0
-    for sentence in df_wplist:
-        vs=analyzer.polarity_scores(sentence)
-        vader.sentimentList.append(vs)
-        scores=("Sentence {}: {}".format(counter, str(vs)))
-        print(scores + " " )
-        counter += 1
-
-
-if __name__ == "__main__":
-    main()
+        counter = 0
+        for sentence in df_wplist:
+            vs = analyzer.polarity_scores(sentence)
+            vader.sentimentList.append(vs)
+            scores = ("Sentence {}: {}".format(counter, str(vs)))
+            print(scores + " " )
+            counter += 1
 
